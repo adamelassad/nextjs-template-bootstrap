36d2f3c0d34d88fe962d50f63ed0c8bff6988eee
adamelassad <elassadadam11@gmail.com>
2025-05-18 17:12:53 +0100

    Add GogoRide/src/User.java 1747584773226

---
+++ GogoRide/src/User.java
@@ -0,0 +1,29 @@
+public class User {
+    private String username;
+    private String password;
+    private String fullName;
+    private String phoneNumber;
+
+    public User(String username, String password, String fullName, String phoneNumber) {
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
